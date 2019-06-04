551426337927bae576f9cdf979dbf05d10d099ac
saurabh <saurabh@gmail.com>
2019-06-04 12:52:31 +0530

    maven project is saved

---
+++ src/main/java/assignmentwebserver/App.java
@@ -0,0 +1,13 @@
+package assignmentwebserver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
